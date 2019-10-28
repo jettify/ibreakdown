aa96157eb3350ff9b0d30dfdf3a6e979e8d5b666
Nikolay Novik <nickolainovik@gmail.com>
2019-10-27 20:33:05 -0400

    Bump version

--- ibreakdown/__init__.py
+++ ibreakdown/__init__.py
@@ -1,5 +1,5 @@
 from .explainer import ClassificationExplainer, RegressionExplainer
 
 
-__version__ = '0.0.1a3'
+__version__ = '0.0.1a4'
 __all__ = ('ClassificationExplainer', 'RegressionExplainer')
