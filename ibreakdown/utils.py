1a0dc2dfe851d6791e20335e03e4d837c2f6217a
Nikolay Novik <nickolainovik@gmail.com>
2019-11-24 15:30:30 -0500

    Fix classificaton explainer (#43)

--- ibreakdown/utils.py
+++ ibreakdown/utils.py
@@ -1,10 +1,6 @@
 import numpy as np
 
 
-def multiply_row(row, num_rows):
-    return np.repeat(row, repeats=num_rows, axis=0)
-
-
 def normalize_array(instance):
     return instance.reshape(1, -1)
 
